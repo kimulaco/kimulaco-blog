6b6c19433d39fbcdd8b224752aad1be50501b8f9
kimulaco <kmrkdev@gmail.com>
2020-04-10 01:14:15 +0900

    feat: add about link

--- src/components/layout/SiteFooter.vue
+++ src/components/layout/SiteFooter.vue
@@ -1,6 +1,12 @@
 <template>
   <footer class="SiteFooter">
     <SiteInner class="SiteFooter_inner">
+      <ul class="SiteFooter_menu">
+        <li class="SiteFooter_menu-item">
+          <nuxt-link class="SiteFooter_menu-link" to="/about/">About</nuxt-link>
+        </li>
+      </ul>
+
       <p class="SiteFooter_copyright">
         <small class="SiteFooter_copyright-text">
           &copy; 2020 kimulaco.
@@ -29,19 +35,34 @@ export default Vue.extend({
   @include media() {
     margin: 30px 0 0;
   }
-  &_inner {
-    @include media() {
-      padding: 0 20px;
-    }
+}
+.SiteFooter_inner {
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  @include media() {
+    padding: 0 20px;
   }
-  &_copyright {
-    text-align: center;
-    @include media() {
-      text-align: left;
-    }
-    &-text {
-      font-size: 14px;
-    }
+}
+.SiteFooter_menu {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  list-style: none;
+}
+.SiteFooter_item {
+  margin: 0 0 0 16px;
+}
+.SiteFooter_copyright {
+  margin: 0;
+  text-align: center;
+  @include media() {
+    text-align: left;
   }
+}
+.SiteFooter_copyright-text {
+  font-size: 14px;
 }
 </style>
