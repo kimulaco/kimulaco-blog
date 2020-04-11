392a986f6bb901d5f4ff5dc99e98a8c87988618c
kimulaco <kmrkdev@gmail.com>
2020-04-11 15:04:20 +0900

    fix: feed url

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -5,7 +5,7 @@ export const SITE_URL = 'https://blog.kimulaco.me'
 export const SITE_THEME_COLOR = '#ffffff'
 export const FACEBOOK_APP_ID = '216629989749363'
 export const COPYRIGHT = '@ 2020 kimulaco.'
-export const FEED_PATH = '/feed.xml'
+export const FEED_PATH = '/feed/post.xml'
 export const FEED_URL = SITE_URL + FEED_PATH
 export const SITEMAP_PATH = '/sitemap.xml'
 
